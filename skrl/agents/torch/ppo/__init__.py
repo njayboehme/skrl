fd3bbb9cabdd89e0a63803423deda469e3b38ed9
njayboehme <njboehme@gmail.com>
2026-07-02 17:18:58 -0700

    Updated init

--- skrl/agents/torch/ppo/__init__.py
+++ skrl/agents/torch/ppo/__init__.py
@@ -1,6 +1,3 @@
 from skrl.agents.torch.ppo.ppo import PPO
 from skrl.agents.torch.ppo.ppo_cfg import PPO_CFG
 from skrl.agents.torch.ppo.ppo_rnn import PPO_RNN
-
-from skrl.agents.torch.ppo.ppo_max import PPO_Max
-from skrl.agents.torch.ppo.ppo_cfg_max import PPO_CFG_MAX
